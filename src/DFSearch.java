c6bee1e45ef8a8ce9cf9a930e3f3fd1259debcae
Pradeep <pradepreddy1993@gmail.com>
2020-06-21 18:18:27 -0500

    inital  commit  : DFS

---
+++ src/DFSearch.java
@@ -0,0 +1,89 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class DFSearch {
+
+// Java program to print DFS traversal from a given given graph
+
+    // This class represents a directed graph using adjacency list
+// representation
+
+        private int V;   // No. of vertices
+
+        // Array  of lists for Adjacency List Representation
+        private LinkedList<Integer> adj[];
+
+        // Constructor
+        DFSearch(int v)
+        {
+            V = v;
+            adj = new LinkedList[v];
+            for (int i=0; i<v; ++i)
+                adj[i] = new LinkedList();
+        }
+
+        //Function to add an edge into the graph
+        void addEdge(int v, int w)
+        {
+            adj[v].add(w);    // Add w to v's list.
+        }
+
+        // A function used by DFS
+        void DFSUtil(int v,boolean visited[])
+        {
+            // Mark the current node as visited and print it
+            visited[v] = true;
+            System.out.println(v+" ");
+
+            // Recur for all the vertices adjacent to this vertex
+            Iterator<Integer> i = adj[v].listIterator();
+            while (i.hasNext())
+            {
+                int n = i.next();
+                if (!visited[n])
+                    DFSUtil(n,visited);
+            }
+        }
+
+        // The function to do DFS traversal. It uses recursive DFSUtil()
+        void DFS()
+        {
+            // Mark all the vertices as not visited(set as
+            // false by default in java)
+            boolean visited[] = new boolean[V];
+            int i=0;
+            // Call the recursive helper function to print DFS traversal
+            // starting from all vertices one by one
+            for (i=0; i<V; ++i)
+
+                if (visited[i] == false) {
+                    DFSUtil(i, visited);
+                    System.out.println("====");
+                }
+           if(i>0)
+           {
+               System.out.println("Graph disconnected");
+           }
+        }
+
+        public static void main(String args[])
+        {
+            DFSearch g = new DFSearch(13);
+
+            g.addEdge(0, 1);
+            g.addEdge(0, 2);
+            g.addEdge(1, 2);
+            g.addEdge(2, 0);
+            g.addEdge(2, 3);
+            g.addEdge(3, 3);
+            g.addEdge(11, 12);
+            g.addEdge(4, 10);
+
+            System.out.println("Following is Depth First Traversal");
+
+            g.DFS();
+        }
+    }
+
+
+
